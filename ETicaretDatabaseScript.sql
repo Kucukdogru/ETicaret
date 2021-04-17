8dc7df686e8835be6207915e8c37a2f137d09165
Merve Kucukdogru <33333935+Kucukdogru@users.noreply.github.com>
2021-04-17 11:23:53 +0300

    Update ETicaretDatabaseScript.sql

--- ETicaretDatabaseScript.sql
+++ ETicaretDatabaseScript.sql
@@ -1,5 +1,6 @@
 CREATE DATABASE ETicaret
-use ETicaret
+USE ETicaret
+
 
 CREATE TABLE Customer(
 Id bigint primary key identity,
@@ -29,4 +30,68 @@ Id bigint primary key identity,
 BrandName nvarchar(100) not null,
 CreateDate datetime not null default getdate(),
 Active bit not null default 1
+)
+
+
+CREATE TABLE Category(
+Id bigint primary key identity,
+CategoryName nvarchar(100),
+CreateDate datetime not null default getdate(),
+Active bit default 1
+)
+
+
+CREATE TABLE Profile(
+CustomerId int primary key,
+Name nvarchar(50) not null,
+Surname nvarchar(50) not null,
+Email varchar(50) not null,
+Phone char(15),
+CreateDate datetime not null default getdate(),
+Active bit not null default 1
+)
+
+
+CREATE TABLE Product(
+Id bigint primary key identity,
+CategoryId bigint not null,
+BrandId bigint not null,
+ProductName nvarchar(50) not null,
+ProductCode varchar(15),
+Price decimal,
+CreateDate datetime not null default getdate(),
+Active bit not null default 1,
+
+Constraint fk_product_category Foreign Key(CategoryId) References Category(Id),
+Constraint fk_product_brand Foreign Key(BrandId) References Brand(Id)
+)
+
+
+
+CREATE TABLE Basket(
+Id bigint primary key identity,
+ProductId bigint not null,
+CustomerId bigint not null,
+Count int not null,
+CreateDate datetime not null default getdate(),
+Active bit not null default 1,
+
+Constraint fk_basket_product Foreign Key(ProductId) References Product(Id),
+Constraint fk_basket_customer Foreign Key(CustomerId) References Customer(Id),
+)
+
+
+CREATE TABLE Order_(
+Id bigint primary key identity,
+ProductId bigint not null,
+CustomerId bigint not null,
+AddressId bigint not null,
+Count int not null,
+Status bit not null default 1,
+CreateDate datetime not null default getdate(),
+Active bit not null default 1,
+
+Constraint fk_order_product Foreign Key(ProductId) References Product(Id),
+Constraint fk_order_customer Foreign Key(CustomerId) References Customer(Id),
+Constraint fk_order_address Foreign Key(AddressId) References Address(Id)
 )
